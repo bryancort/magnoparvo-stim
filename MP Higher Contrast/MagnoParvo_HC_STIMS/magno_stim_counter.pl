b4e805e80533b706f47344b9416d53c855c92bd5
Gaelen Hadlett <gaelenh@gmail.com>
2013-03-27 17:43:14 -0400

    Added high contrast Presentation code

---
+++ MP Higher Contrast/MagnoParvo_HC_STIMS/magno_stim_counter.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+# Count the number of stimuli in the magno list.
+
+open(IN, "<dmmagn.lst") || die;
+my($scroll, $targ, $sat); #sat = scroll after target
+my($num_trial);
+my(@line);
+
+foreach $line (<IN>) {
+	@line = split(/\t/, $line);
+	if ($line[0] !~ /^\d/) {
+		next;
+	} else {
+		$num_trials++;
+	}
+	if ($line[0] == 11) {
+		$scroll++;
+	} elsif ($line[0] == 101) {
+		if ($line[2] == 8) {
+			$sat++;
+		} else {
+			$targ++;
+		}
+	}
+}
+
+print "Total Trials:\t$num_trials\n";
+print "Total Targets:\t$targ\t% Targets:\t";
+printf("%.2f\n", $targ/$num_trials * 100);
+print "Total Scrolls:\t$scroll\t% Scrolls:\t";
+printf("%.2f\n", $scroll/$num_trials * 100);
+print "Total SATs:\t$sat\t% SATs\t\t";
+printf("%.2f\n", $sat/$num_trials * 100);
+<STDIN>;
---
+++ MP Higher Contrast/Makemp.sce
@@ -0,0 +1,11 @@
+# make magno parvo files with fixed isis 
+
+pcl_file = "makemp.pcl";
+default_background_color = 0,0,0;
+
+begin; 
+
+picture {} default;
+
+trial {
+} theTrial;
