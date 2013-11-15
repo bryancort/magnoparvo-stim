5b3798c08166ab64d93a0ec7733b0426a41795a7
Gaelen Hadlett <gaelenh@gmail.com>
2013-11-15 00:38:03 -0500

    Added controllers module

---
+++ controllers.py
@@ -0,0 +1,73 @@
+
+# vendor
+import pyxid
+from psychopy.core import Clock
+from psychopy import event
+
+# local
+import utils
+
+
+class Controller(object):
+
+    def wait_for_response(self, buttons=None, timeout=None):
+        raise NotImplementedError
+
+
+class Keyboard(Controller):
+    pass
+
+
+class Mouse(Controller):
+    pass
+
+
+class Cedrus(Controller):
+
+    def __init__(self):
+        devices = pyxid.get_xid_devices()
+        self._device = devices[0]
+        if self._device.is_response_device():
+            self._device.reset_base_timer()
+            self._device.reset_rt_timer()
+
+    def wait_for_response(self, buttons=None, timeout=None):
+        buttons = utils.as_list(buttons)
+        # clear queue here
+
+        if timeout:
+            timer = Clock()
+            check = lambda: timer.getTime() < timeout
+        else:
+            check = lambda: True
+
+        while check():
+            self._device.poll_for_response()
+            if self._device.response_queue_size() > 0:
+                response = self._device.get_next_response()
+                if (buttons and response in buttons) or (not buttons and response):
+                    return True
+
+
+_LAB_CONTROLLERS = {
+    'cedrus': {
+        'class': Cedrus,
+        'params': {}
+    },
+    'keyboard': {
+        'class': Keyboard,
+        'params': {}
+    },
+    'mouse': {
+        'class': Mouse,
+        'params': {}
+    }
+}
+
+
+def get(name):
+    name = name.lower()
+    if name not in _LAB_CONTROLLERS:
+        raise ValueError("Unknown controllers '%s'. Expecting %s" % _LAB_CONTROLLERS.keys())
+    contr = _LAB_CONTROLLERS[name]()
+    return contr
--- magno-psychopy.py
+++ magno-psychopy.py
@@ -11,6 +11,7 @@ import egi.simple as egi
 # import egi.threaded as egi
 
 # local
+import controllers
 import monitors
 import utils
 
@@ -28,12 +29,22 @@ class Experiment(object):
         self._monitor = None
         self._window = None
         self._netstation = None
-        self._control_box = ControlBox()
+        self._controller = None
+        self._debug = debug
 
         if debug:
             logging.console.setLevel(logging.DEBUG)
         else:
-            logging.console.setLevel(logging.INFO)
+            logging.console.setLevel(logging.WARNING)
+
+    def init_display(self, monitor, width=1920, height=1200):
+        self._monitor = monitors.get(monitor)
+        self._window = visual.Window(size=[width, height],
+                                     monitor=self._monitor,
+                                     allowGUI=False)
+
+    def init_controller(self, controller):
+        self._controller = controllers.get(controller)
 
     def start_netstation(self):
         self._netstation = egi.Netstation()
@@ -63,12 +74,6 @@ class Experiment(object):
                 pass
             self._window.flip()
 
-    def init_display(self, monitor, width=1920, height=1200):
-        self._monitor = monitors.get(monitor)
-        self._window = visual.Window(size=[width, height],
-                                     monitor=self._monitor,
-                                     allowGUI=False)
-
     def load_still_images(self, dirpath, pos=(0.0, 0.0)):
         images = []
         for fname in os.listdir(dirpath):
@@ -81,7 +86,7 @@ class Experiment(object):
         return visual.ImageStim(self._window, image=fpath, pos=pos)
 
     def wait_for_response(self, buttons=None):
-        return self._control_box.wait_for_buttons()
+        return self._control_box.wait_for_response()
 
     def run(self):
         raise NotImplementedError()
@@ -89,20 +94,6 @@ class Experiment(object):
     def release(self):
         self._windows.close()
         core.quit()
-
-
-class ControlBox(object):
-
-    def __init__(self):
-        pass
-
-    def connect(self):
-        pass
-
-    def wait_for_buttons(self, buttons=None, timeout=None):
-        buttons = utils.as_list(buttons)
-        time.sleep(6)
-        return True
 
 
 class Magno(Experiment):
@@ -116,27 +107,28 @@ class Magno(Experiment):
 
         horizontal_sine = visual.GratingStim(self._window,
                                              tex='sin',
+                                             texRes=256,
                                              units='deg',
                                              sf=1.5,
                                              size=2)
-        self.start_netstation()
+        horizontal_sine.setUseShader(True)
 
-        clock = core.Clock()
+        self.start_netstation()
 
         def move():
             # horizontal_sine.setPhase(4*t)
             horizontal_sine.setPhase(0.06666666666666667, '+')
             horizontal_sine.draw()
-        
+
         def still():
             horizontal_sine.draw()
 
         self.timed_func(60 * 3, still)
-        
+
         for current in plan:
             if not isinstance(current, int):
                 # Show image
-                self.timed_func(500, current.draw)
+                self.timed_func(15, current.draw)
 
                 # Wait for the response
                 self.wait_for_response()
@@ -148,28 +140,12 @@ class Magno(Experiment):
                 self.send_event('move', label="moved", description='Sine grating finished moving', table={'frms': post_cartoon})
             else:
                 # Move for 100 ms / 6 frames
-                now = clock.getTime()
                 self.timed_func(6, move)
                 self.send_event('stop', label="stopped", description='Sine grating finished pausing', table={'frms': 6})
-                now2 = clock.getTime()
-                
+
                 # Pause for however long plan says
                 self.timed_func(current, still)
-
                 self.send_event('move', label="moved", description='Sine grating finished moving', table={'frms': current})
-                now3 = clock.getTime()
-                
-                expected =  0.016666666666666667
-                avg1 = (now2-now) / 6
-                avg2 = (now3-now2) / current
-                if avg1 > expected:
-                    print '6 frame avg diff', abs(avg1 - expected) * 1000
-                else:
-                    print '6 frame avg diff', abs(expected - avg1) * 1000
-                if avg2 > expected:
-                    print '%s frame avg diff ' % current, 'diff is', abs(avg2 - expected) * 1000
-                else:
-                    print '%s frame avg diff' % current, 'diff is', abs(expected - avg2) * 1000
 
         self.stop_netstation()
 
@@ -177,4 +153,5 @@ class Magno(Experiment):
 if __name__ == '__main__':
     exp = Magno()
     exp.init_display('run-station', 800, 600)
-    exp.run()
+    exp.init_controller('cedrus')
+    exp.run(debug=True)
